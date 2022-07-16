1d242d1e063d14b36db8eab3083209392d3b52c3
zhaoxiaogang <286505491@qq.com>
2022-07-16 23:10:41 +0800

    log impl;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+use log::{info, error};
+use log4rs;
+
+
 fn main() {
     println!("Hello, world!");
+
+    log4rs::init_file("config/config.yaml", Default::default()).unwrap();
+    info!("hnt tools booting up");
+    error!("Connecting to database...");
 }
