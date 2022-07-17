c50db893b9ea82294ad245e343f3230c6ac0ba5b
zhaoxiaogang <286505491@qq.com>
2022-07-17 22:45:15 +0800

    add framewoker;

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rbatis;
+#[macro_use]
+extern crate lazy_static;
 
 use log::{info, error};
 use log4rs;
 
+// 定义全局变量
+lazy_static! {
+    // Rbatis类型变量 RB，用于数据库查询
+    static ref RB: Rbatis = Rbatis::new();
+}
 
-fn main() {
+#[rocket::main]
+async fn main() {
     println!("Hello, world!");
 
     log4rs::init_file("config/config.yaml", Default::default()).unwrap();
